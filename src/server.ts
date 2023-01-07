09b41d2b0ad119e60fc96520226c0157013c5ebb
Joey Quadros <joey_qpb@hotmail.com>
2023-01-07 11:07:13 -0300

    feat: add express inital server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("hello peter!");
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.send("oi");
+});
+
+app.listen(Number(process.env.PORT), () => {
+  console.log(`Server running and up at port: ${process.env.PORT}`);
+});
