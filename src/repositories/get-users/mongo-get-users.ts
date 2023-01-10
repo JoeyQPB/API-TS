0c14609ca54ee07fe77e1209667c18eecb6964cc
Joey Quadros <joey_qpb@hotmail.com>
2023-01-10 18:04:40 -0300

    feat: ensure GetUsersMongoRepository gets users from Mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,10 +1,17 @@
 import { GetUsersRepositoryInterface } from "../../controllers/get-users.ts/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements GetUsersRepositoryInterface {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
     return [
       {
+        id: "01",
         name: "joey",
         email: "joey@ex.com",
         telefone: 77999862226,
@@ -16,6 +23,7 @@ export class MongoGetUsersRepository implements GetUsersRepositoryInterface {
         },
         cpf: "00000000000",
       },
+      ...users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() })),
     ];
   }
 }
